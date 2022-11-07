ea4f408c2be0561d1a201e3188f75399652800c2
KNDARK <knofficial6@gmail.com>
2022-11-07 20:27:37 +0700

    base

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        User x = new User(1, "khuong", true);
+        
+    }
+}
